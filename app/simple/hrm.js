ca5d5b06490fd9e8ed966db62f90a0dbbe5e97c7
Harrison Quarry <harrisonqleddy@gmail.com>
2020-04-21 12:50:00 -0700

    messy

---
+++ app/simple/hrm.js
@@ -0,0 +1,88 @@
+/*
+  Returns the Heart Rate BPM, with off-wrist detection.
+  Callback raised to update your UI.
+*/
+import { me } from "appbit";
+import { display } from "display";
+import { HeartRateSensor } from "heart-rate";
+import { user } from "user-profile";
+
+import * as messaging from "messaging";
+
+let hrm, watchID, hrmCallback;
+let lastReading = 0;
+let heartRate;
+let heartRate_Time = {};
+let hrCounter = 0;
+
+export function initialize(callback) {
+  if (me.permissions.granted("access_heart_rate") && me.permissions.granted("access_user_profile")) {
+    hrmCallback = callback;
+    hrm = new HeartRateSensor();
+    setupEvents(); // subscribed HR monitor to the display -> should deactivate that
+    start();
+    lastReading = hrm.timestamp;
+  } else {
+    console.log("Denied Heart Rate or User Profile permissions");
+    callback({
+      bpm: "???",
+      zone: "denied",
+      restingHeartRate: "???"
+    });
+  }
+}
+
+function sendHeartRate(heartRate) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send({
+      HeartRate: heartRate
+    });
+  }
+    else {
+        console.log("Error sending Heart Rate Data");
+    }
+}
+
+function getReading() {
+  if (hrm.timestamp === lastReading) {
+    heartRate = "--";
+  } else {
+    heartRate = hrm.heartRate;
+    hrCounter++;
+    heartRate_Time[hrm.timestamp] = heartRate;
+  }
+  lastReading = hrm.timestamp;
+  if (hrCounter % 5 == 0){
+    sendHeartRate(JSON.stringify(heartRate_Time));
+    heartRate_Time = {};
+  }
+  hrmCallback({
+    bpm: heartRate,
+    zone: user.heartRateZone(hrm.heartRate || 0),
+    restingHeartRate: user.restingHeartRate
+  });
+}
+
+function setupEvents() {
+  display.addEventListener("change", function() {
+    if (display.on) {
+      start();
+    } else {
+      stop();
+    }
+  });
+}
+
+function start() {
+  if (!watchID) {
+    hrm.start();
+    getReading();
+    watchID = setInterval(getReading, 1000);
+  }
+}
+
+function stop() {
+  hrm.stop();
+  clearInterval(watchID);
+  watchID = null;
+}
---
+++ companion/index.js
@@ -0,0 +1,43 @@
+import * as simpleSettings from "./simple/companion-settings";
+
+simpleSettings.initialize();
+
+import * as messaging from "messaging";
+// const WebSocket = require('ws');
+// let socket;
+// const socket = new WebSocket('wss://192.168.1.223:8876');
+// socket.close();
+
+// socket.addEventListener("open", onOpen);
+// socket.addEventListener("error", onError);
+
+function onOpen(evt) {
+    console.log("CONNECTED");
+}
+
+function onError(evt) {
+    console.log("ERROR: ${evt.data}");
+}
+
+function sendHeartRate(data) {
+    console.log(data);
+    // console.log("Sending Heart Rate by http");
+    // fetch('https://192.168.1.223:8876', {
+    //     method: "POST",
+    //     // headers: {
+    //     //     'Content-Type': 'application/json',
+    //     // },
+    //     body: JSON.stringify(data),
+    // })
+    //     .then((response) => response.json())
+    //     .then((data) => console.log("Success ", data))
+    //     .catch((error) => console.error("Error: ", error));
+}
+
+messaging.peerSocket.onmessage = function(event) {
+    // console.log("Data recieved from app");
+    if(event.data) {
+        sendHeartRate(event.data.HeartRate);
+    }
+}
+
